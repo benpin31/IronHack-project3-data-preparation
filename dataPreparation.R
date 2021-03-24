1bb0e0fae9a6e8cea91e2bae070a2b0438971101
Benjamin Pinard <benpin31@gmail.com>
2021-03-24 13:59:25 +0100

    seed for 2019, 202, 2021

--- dataPreparation.R
+++ dataPreparation.R
@@ -38,25 +38,61 @@ colnames(stat) <- c(
 
 stat <- stat[!is.na(timeRange)]
 
+# data 2019
 meltStat <- as.data.table(melt(stat, id.vars=c("date", "timeRange", "Physical", "Telephone"))[!is.na(value)])
+meltStat[,date := as.POSIXct(date, format="%d/%m/%Y")]
+
+# data 2020
+meltStat2020=copy(meltStat)
+meltStat2020[, date:=date+364*24*3600 ]
+meltStat2020<-meltStat2020[format(date,"%Y-%m-%d") != "2020-01-01"]
+meltStat2020[,mult := rnorm(.N,1.1,0.1)]
+meltStat2020[, Physical := floor(Physical*mult)]
+meltStat2020[, Telephone := floor(Telephone*mult)]
+meltStat2020[, value := floor(value*mult)]
+meltStat2020[,mult:=NULL]
+
+# data 2021
+meltStat2021=copy(meltStat)
+meltStat2021[, date:=date+(365+363)*24*3600 ]
+meltStat2021<-meltStat2021[!(format(date,"%Y-%m-%d") %in% c("2021-01-01", "2020-12-30", "2020-12-31"))]
+meltStat2021[,mult := rnorm(.N,1.3,0.1)]
+meltStat2021[, Physical := floor(Physical*mult)]
+meltStat2021[, Telephone := floor(Telephone*mult)]
+meltStat2021[, value := floor(value*mult)]
+meltStat2021[,mult:=NULL]
+
+# total
+meltStat[date == "2021-11-29"]
+meltStat2020[date == "2021-11-29"]
+meltStat2021[date == "2021-11-29"]
+
+meltStat <- rbind(meltStat, meltStat2020, meltStat2021)[order(date)]
+meltStat[date == "2021-12-28"]
+meltStat2021[date == "2021-12-28"]
+meltStat2020[date == "2021-11-29"]
+
+
 
 randomHour = function(timeRange, date) {
   if(timeRange == "15h30 - 18h00") {
-    return(as.POSIXct(paste0(date, " 15:30"), format='%d/%m/%Y %H:%M') + runif(1, 0, 60*150))
+    return(as.POSIXct(paste0(date, " 15:30"), format='%Y-%m-%d %H:%M') + runif(1, 0, 60*150))
   }
   if(timeRange == "13h00 - 15h30") {
-    return(as.POSIXct(paste0(date, " 13:00"), format='%d/%m/%Y %H:%M') + runif(1, 0, 60*150))
+    return(as.POSIXct(paste0(date, " 13:00"), format='%Y-%m-%d %H:%M') + runif(1, 0, 60*150))
   }
   if(timeRange == "18h00 - 20h00") {
-    return(as.POSIXct(paste0(date, " 18:00"), format='%d/%m/%Y %H:%M') + runif(1, 0, 60*120))
+    return(as.POSIXct(paste0(date, " 18:00"), format='%Y-%m-%d %H:%M') + runif(1, 0, 60*120))
   }
 }
 
 # Create JSON
 
 res = c()
+titi <- 1
 
-for (dateValue in unique(meltStat[, date])) {
+for (dateValue in unique(meltStat[, format(date,"%Y-%m-%d" )])) {
+  print(dateValue)
   for (timeRangeValue in unique(meltStat[date == dateValue, timeRange])) {
     segment <- meltStat[date == dateValue & timeRange == timeRangeValue]
     nbPhys <- head(segment[, Physical],1)
@@ -78,6 +114,7 @@ for (dateValue in unique(meltStat[, date])) {
       }
     }
   }
+  titi <- titi+1
 }
 
 
@@ -85,15 +122,23 @@ res <- res[,list(variable, V2, V3)]
 colnames(res) <- c("category", "date", "contactType")
 
 res[, seed := paste0('{"category":"',category,'", "date":"',date,'", "contactType":"',contactType,'"}')]
-res2020 <- copy(res)
-res2020[, date := date+364*24*3600]
-res2020 <- res2020[format(date,"%Y-%m-%d") != "2020-01-01"]
-sort(res2020[,unique(format(date,"%Y-%m-%d"))])
-res2021 <- copy(res)
-res2021[, date := date+(365+363)*24*3600]
-res2021 <- res2021[!(format(date,"%Y-%m-%d") %in% c("2019-01-01", "2020-12-30", "2020-12-31"))]
-sort(res2021[,unique(format(date,"%Y-%m-%d"))])
 
 querySeed <- paste0("[", paste(res[,seed], collapse=",\n"), "]")
 write.csv(querySeed, "seed.txt", quote=FALSE)
+
+
+
+# data viz example
+
+barplot(res[,.N, by = format(date, "%Y-%m")][, N])
+res[,]
+
+
+
+
+
+
+
+
+
 
